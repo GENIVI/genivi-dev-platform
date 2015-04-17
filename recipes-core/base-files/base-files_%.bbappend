GENIVI/genivi-dev-platform

commit 2ce2a6feac3f4daad4410bffbc4253326c528958

GDP: create persitence mounting point
for persistence support we need to get
 /Data/mnt-c and /Data/mnt-wt
and we modify fstab to have those monting point onto the sdcard mmc1

recipes-core/base-files/base-files_%.bbappend
@@ -1,3 +1,8 @@
 do_configure_append() {
        echo "\033[9;0]" >> ${WORKDIR}/issue
+
+       echo "/dev/mmcblk1p1       /Data/mnt-c           auto       defaults 0 2" >> ${WORKDIR}/fstab
+       echo "/dev/mmcblk1p1       /Data/mnt-wt          auto       defaults 0 2" >> ${WORKDIR}/fstab
 }
+
+dirs755 += " /Data/mnt-c /Data/mnt-wt"

commit d0f5baa9b8de89bfa392f9855a99a2053ba14be5

GDP: fix issue when booting without second SDcard
for persistence support we are using second sd-card. Unfortunately, in case the
second sd card is not present, the system is no more booting.

This commit fix problem introduced by commit
commit 2ce2a6feac3f4daad4410bffbc4253326c528958
    GDP: create persitence mounting point

Now before starting launcher we test if the sdcard is present or not and then
mount expected mounted directory only in the case it is present.

recipes-core/base-files/base-files_%.bbappend
@@ -1,8 +1,5 @@
 do_configure_append() {
        echo "\033[9;0]" >> ${WORKDIR}/issue
-
-       echo "/dev/mmcblk1p1       /Data/mnt-c           auto       defaults 0 2" >> ${WORKDIR}/fstab
-       echo "/dev/mmcblk1p1       /Data/mnt-wt          auto       defaults 0 2" >> ${WORKDIR}/fstab
 }
 
 dirs755 += " /Data/mnt-c /Data/mnt-wt"

recipes-demo-hmi/genivi-demo-platform-hmi/gdp-hmi-launcher2/start_launcher.sh
@@ -1,3 +1,11 @@
 #! /bin/sh
-/bin/mkdir /Data/mnt-c/Launcher
+device0="/dev/mmcblk1p1"
+
+if [ -b "$device0" ]
+then
+    /bin/mount -t auto /dev/mmcblk1p1 /Data/mnt-c
+    /bin/mount -t auto /dev/mmcblk1p1 /Data/mnt-wt
+    /bin/mkdir /Data/mnt-c/Launcher
+fi
+
 /bin/systemctl start --user gdp-hmi-launcher2.service