2afeaf49f18929d141b18d1efa7b21253008233e
Gunnar Andersson <gand@acm.org>
2015-08-12 17:02:36 +0200

    Ensure init.sh is being sourced + remove template
    Copying yocto conf template is no longer used since
    a configuration is provided already.
    Therefore, remove TEMPLATE stuff from the script.

--- init.sh
+++ init.sh
@@ -9,9 +9,13 @@ git submodule init
 # is better (because they are not that user friendly otherwise...))
 git submodule update 
 
-cd meta-genivi-demo/conf
-export TEMPLATECONF=`pwd`
-cd -
+# Check that it was sourced
+cmd=$(basename "$0")
+[ "$cmd" = "init.sh" ] && { 
+   echo "No, the script needs to be _sourced_ from your current shell." 
+   echo "Use source $cmd  or  . $cmd" 
+   exit 1
+}
 
 source poky/oe-init-build-env gdp-src-build
 
