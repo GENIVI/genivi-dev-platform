3080a132463f53ca23cadc7744a6c45330e0b90d
Gunnar Andersson <gand@acm.org>
2015-08-12 16:56:46 +0200

    Improve the script

--- init.sh
+++ init.sh
@@ -1,6 +1,13 @@
 # This script should be sourced 
 
+# init really makes sense only the first time
+# and after that is redundant
 git submodule init
+
+# update here could help ensure people get the right checked out version
+# after they have switched branches. However learning how submodules work
+# is better (because they are not that user friendly otherwise...))
+git submodule update 
 
 cd meta-genivi-demo/conf
 export TEMPLATECONF=`pwd`
