fa0ecd0aeca7289dbbb2120cacd998f169c8bd45
Tom Pollard <tom.pollard@codethink.co.uk>
2016-05-10 10:44:11 +0000

    Initial attempt at a single branch PoC approach

--- init.sh
+++ init.sh
@@ -8,6 +8,38 @@ cmd=$(basename "$0")
    exit 1
 }
 
+# User to select a target board
+
+while test -z "$machine" ; do
+
+    echo -e "Enter target; 'qemux86-64', 'porter', 'raspberrypi2', 'minnowboard': \c "
+
+    read choice
+
+    echo "You selected target $choice"
+
+    declare -a targets=("qemux86-64" "porter" "raspberrypi2" "minnowboard")
+
+    for i in ${targets[@]}; do
+        if [[ ${i} == $choice ]] ; then
+            machine=$choice
+	    echo "$machine is a valid target"
+            break
+        fi
+    done
+    if test -z "$machine" ; then
+        echo "An invalid target name was given of $choice, please enter valid target"
+    fi
+done
+
+cp gdp-src-build/conf/templates/${machine}.local.conf gdp-src-build/conf/local.conf
+cp gdp-src-build/conf/templates/${machine}.bblayers.conf gdp-src-build/conf/bblayers.conf
+echo "Local & bblayers conf set for $machine"
+
+# Unset variables
+unset machine
+unset choice
+
 # init really makes sense only the first time
 # and after that is redundant
 git submodule init
