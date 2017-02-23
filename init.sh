17fa5cbe97a0d52597b5944eef9b27f3ae8b3b21
Stephen Lawrence <stephen.lawrence@renesas.com>
2017-02-23 14:52:57 +0000

    conf: Setup for the Renesas R-Car M3 Starter Kit
    Extend the GDP initialisation mechanism to support the Renesas R-Car
    M3 Starter Kit [1]. A low cost board using the M3 SoC from the Renesas
    R-Car Gen 3 family.
    
    [1] http://www.elinux.org/R-Car/Boards/M3SK
    
    Signed-off-by: Stephen Lawrence <stephen.lawrence@renesas.com>

--- init.sh
+++ init.sh
@@ -17,8 +17,8 @@ fi
 
 echo "You selected target $choice"
 
-declare -a targets=("qemux86-64" "porter" "raspberrypi2" "raspberrypi3" "minnowboard" "silk" "dragonboard-410c")
-declare -a supported=("qemux86-64" "porter" "minnowboard" "raspberrypi2" "raspberrypi3" "dragonboard-410c" "silk")
+declare -a targets=("qemux86-64" "porter" "raspberrypi2" "raspberrypi3" "minnowboard" "silk" "dragonboard-410c" "r-car-m3-starter-kit")
+declare -a supported=("qemux86-64" "porter" "minnowboard" "raspberrypi2" "raspberrypi3" "dragonboard-410c" "silk" "r-car-m3-starter-kit")
 declare -a variables=("choice" "eula" "machine" "modules" "bsp" "bsparr" "supported" "targets" "variables")
 
 for i in ${targets[@]}; do
@@ -74,7 +74,7 @@ echo "Local & bblayers conf set for $machine"
 # bsp submodule layer
 declare -A bsparr
 
-bsparr+=( ["minnowboard"]="meta-intel" ["raspberrypi2"]="meta-raspberrypi" ["raspberrypi3"]="meta-raspberrypi" ["porter"]="meta-renesas" ["silk"]="meta-renesas" ["dragonboard-410c"]="meta-qcom" )
+bsparr+=( ["minnowboard"]="meta-intel" ["raspberrypi2"]="meta-raspberrypi" ["raspberrypi3"]="meta-raspberrypi" ["porter"]="meta-renesas" ["silk"]="meta-renesas" ["dragonboard-410c"]="meta-qcom" ["r-car-m3-starter-kit"]="renesas-rcar-gen3")
 
 modules=($(git submodule | awk '{ print $2 }'))
 for i in ${bsparr[@]}; do
