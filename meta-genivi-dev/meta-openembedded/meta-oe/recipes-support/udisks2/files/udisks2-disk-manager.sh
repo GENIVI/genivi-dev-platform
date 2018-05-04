7148d5c46d06ace1e419678ea7d10e48a4ab9d30
Mirza Krak <mirza.krak@endian.se>
2018-05-04 12:45:48 +0000

    Add udisks2 based service to automount storage devices
    A sample services that simply uses udev monitor and udisksctl to mount USB/MMC
    storage devices based on hotplug events.
    
    [GDP-252] Add support for udisks2 to the GDP image
    
    Signed-off-by: Mirza Krak <mirza.krak@endian.se>

---
+++ meta-genivi-dev/meta-openembedded/meta-oe/recipes-support/udisks2/files/udisks2-disk-manager.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# Script from https://wiki.archlinux.org/index.php/udisks
+
+pathtoname() {
+    udevadm info -p /sys/"$1" | awk -v FS== '/DEVNAME/ {print $2}'
+}
+
+stdbuf -oL -- udevadm monitor --udev -s block | while read -r -- _ _ event devpath _; do
+        if [ "$event" = add ]; then
+            devname=$(pathtoname "$devpath")
+            udisksctl mount --block-device "$devname" --no-user-interaction
+        fi
+done
